533736fa1582ccf7fe33e18de2ee26b80795d845
dfano <davidfano@gmail.com>
2019-04-02 20:17:04 -0400

    commit initial push of app

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController # i just copied and pasted and thats why it broke. it needed to say "Application"
+	def about
+	end
+
+	def home
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'pages/about', to: 'pages#about'
+  get 'pages/home', to: 'pages#home'
+
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users , except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+ 
+end
